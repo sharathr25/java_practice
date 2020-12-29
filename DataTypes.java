bfee4b384a684a9ceaf86e7aad503abed20daa22
sharathr25 <sharathdupati@gmail.com>
2020-12-29 18:07:34 +0530

    tut 1: Variables and data types

---
+++ DataTypes.java
@@ -0,0 +1,21 @@
+class DataTypes {
+    public static void main(String args[]) {
+        byte myAge = 24;
+        short prizeOfTv = 20000;
+        int noOfPostsApplied = 300000;
+        long mobileNumber = 9886739011L;
+        float pi = 3.1415F;
+        double tenByThree = 3.333333333333333;
+        boolean isOneLessThanTwo = true;
+        char firstLetterOfAlphabets = 'A';
+
+        System.out.println(myAge);
+        System.out.println(prizeOfTv);
+        System.out.println(noOfPostsApplied);
+        System.out.println(mobileNumber);
+        System.out.println(pi);
+        System.out.println(tenByThree);
+        System.out.println(isOneLessThanTwo);
+        System.out.println(firstLetterOfAlphabets);
+    }
+}
